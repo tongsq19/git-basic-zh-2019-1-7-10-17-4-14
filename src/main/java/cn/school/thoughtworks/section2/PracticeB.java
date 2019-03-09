8a8e2fe299eaeaefc3135e175ebe5adcb68a4c14
tongsq19 <tongsq14@mails.tsinghua.edu.cn>
2019-03-10 01:43:45 +0800

    Pass PracticeB Test in Section2

--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -1,12 +1,32 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
     Map<String, Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
+        HashMap<String, Integer> stringCountMap = new HashMap<>();
+        for (String s: collection1) {
 
-        return null;
+            int index = s.indexOf('-');
+            if (index == -1) {
+                addCount(stringCountMap, s, 1);
+            } else {
+                String key = s.substring(0, index);
+                int value = Integer.parseInt(s.substring(index + 1));
+                addCount(stringCountMap, key, value);
+            }
+        }
+        return stringCountMap;
+    }
+
+    private void addCount(HashMap<String, Integer> stringCountMap, String key, int value) {
+        if (!stringCountMap.containsKey(key)) {
+            stringCountMap.put(key, value);
+        } else {
+            stringCountMap.put(key, stringCountMap.get(key) + value);
+        }
     }
 }
