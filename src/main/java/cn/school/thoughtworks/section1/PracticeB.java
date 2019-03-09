0905c07757bc82b52cec51710460083dc104086e
tongsq19 <tongsq14@mails.tsinghua.edu.cn>
2019-03-10 00:44:18 +0800

    Pass PracticeB Test in Section1

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,21 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        //选出1集合中与2集合中子数组的元素相同的元素
+        List<String> unionSet = new ArrayList<String>();
+
+        for (String s : collection1) {
+            for (List<String> strings : collection2) {
+                if (strings.contains(s)) {
+                    unionSet.add(s);
+                    break;
+                }
+            }
+        }
+        return unionSet;
     }
 }
