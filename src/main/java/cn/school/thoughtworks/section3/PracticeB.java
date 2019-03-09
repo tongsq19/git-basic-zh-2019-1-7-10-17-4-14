45bb47898d5f1c4492c8d09a472be9703a379207
tongsq19 <tongsq14@mails.tsinghua.edu.cn>
2019-03-10 02:25:07 +0800

    Pass PracticeB test for Section3

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -1,12 +1,20 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
-        //实现练习要求，并改写该行代码。
+        Map<String, Integer> updatedCollection = new HashMap<String, Integer>(collectionA);
 
-        return null;
+        List<String> collectionB = object.get("value");
+        for (String s: collectionB) {
+            if(updatedCollection.containsKey(s)) {
+                int newValue = updatedCollection.get(s) - updatedCollection.get(s)/3;
+                updatedCollection.put(s, newValue);
+            }
+        }
+        return updatedCollection;
     }
 }
