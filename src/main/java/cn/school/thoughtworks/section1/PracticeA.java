a108bbf5fab62111196822fd338ec934ad5f67c2
tongsq19 <tongsq14@mails.tsinghua.edu.cn>
2019-03-10 00:35:51 +0800

    Pass PracticeATest in Section1

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,10 +1,18 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        //选出1集合中与2集合中相同的元素
+        List<String> unionSet = new ArrayList<String>();
+        for (String s : collection1) {
+            if (collection2.contains(s)) {
+                unionSet.add(s);
+            }
+        }
+        return unionSet;
     }
 }
+
