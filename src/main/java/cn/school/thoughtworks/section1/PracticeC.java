223838be429f3f634de1031d1c96549229354353
tongsq19 <tongsq14@mails.tsinghua.edu.cn>
2019-03-10 00:48:45 +0800

    Pass PracticeC Test in Section1

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,18 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
-        //实现练习要求，并改写该行代码。
-        return null;
+        //选出1集合中跟2对象中value属性中的元素相同的元素
+        List<String> unionSet = new ArrayList<String>();
+        for (String s : collection1) {
+            if(collection2.get("value").contains(s)) {
+                unionSet.add(s);
+            }
+        }
+        return unionSet;
     }
 }
